ee5a069ffa45eeee950cea2c301014041175d4fe
김두영 <dubblx05@naver.com>
2025-04-24 14:07:34 +0900

    [Init] 프로젝트 초기 세팅

---
+++ backend/FitPtAdmin/src/main/java/com/sahur/fitptadmin/FitPtAdminApplication.java
@@ -0,0 +1,13 @@
+package com.sahur.fitptadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitPtAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitPtAdminApplication.class, args);
+    }
+
+}
