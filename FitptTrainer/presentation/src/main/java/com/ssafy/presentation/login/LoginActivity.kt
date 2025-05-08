e708c080c092da64dd374cbf04d51b8ebaacb063
김동현 <charlie7276@naver.com>
2025-05-08 14:17:48 +0900

    [Feat] 타이틀 바 제거, 상태 바 배경색 및 텍스트 색 변경 #front

--- FitptTrainer/presentation/src/main/java/com/ssafy/presentation/login/LoginActivity.kt
+++ FitptTrainer/presentation/src/main/java/com/ssafy/presentation/login/LoginActivity.kt
@@ -1,6 +1,8 @@
 package com.ssafy.presentation.login
 
 import android.os.Bundle
+import android.view.View
+import android.view.Window
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
