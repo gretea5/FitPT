553bd0d2a87db106e8d5b928f2a4ae1b55827ba0
김동현 <charlie7276@naver.com>
2025-05-10 20:45:24 +0900

    [Style] Design HealthReportFragment - report muscles toggle group button #front

--- FitptTrainer/presentation/src/main/java/com/ssafy/presentation/report/viewpager/HealthReportFragment.kt
+++ FitptTrainer/presentation/src/main/java/com/ssafy/presentation/report/viewpager/HealthReportFragment.kt
@@ -1,10 +1,12 @@
 package com.ssafy.presentation.report.viewpager
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import com.ssafy.presentation.R
 import com.ssafy.presentation.base.BaseFragment
 import com.ssafy.presentation.databinding.FragmentHealthReportBinding
@@ -18,7 +20,20 @@ class HealthReportFragment : BaseFragment<FragmentHealthReportBinding>(
         initEvent()
     }
 
-    fun initEvent(){
+    fun initEvent() {
+        binding.apply {
+            tgReportMuscles.addOnButtonCheckedListener { group, checkedId, isChecked ->
+                val frontButton = binding.btnReportMusclesFront
+                val backButton = binding.btnReportMusclesBack
 
+                if (checkedId == R.id.btn_report_muscles_front && isChecked) {
+                    frontButton.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.highlight_orange))
+                    backButton.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.main_gray))
+                } else if (checkedId == R.id.btn_report_muscles_back && isChecked) {
+                    backButton.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.highlight_orange))
+                    frontButton.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.main_gray))
+                }
+            }
+        }
     }
 }
