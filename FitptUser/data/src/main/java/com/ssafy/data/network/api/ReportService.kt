636153bb586b2172bd698c966665830ebfe95589
김기훈 <y2k16241@naver.com>
2025-05-11 21:58:00 +0900

    Feat: report api 추가

--- FitptUser/data/src/main/java/com/ssafy/data/network/api/ReportService.kt
+++ FitptUser/data/src/main/java/com/ssafy/data/network/api/ReportService.kt
@@ -1,4 +1,23 @@
 package com.ssafy.data.network.api
 
+import retrofit2.http.GET
+import retrofit2.http.Query
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.PATCH
+import retrofit2.http.POST
+import retrofit2.http.Path
+
 interface ReportService {
+    @GET("reports")
+    fun getReportList(@Query("memberId") memberId: Int) :Response<Unit>
+
+    @POST("reports")
+    fun createReport() : Response<Unit>
+
+    @GET("reports/{reportId}")
+    fun getDetailReport(@Path("reportId") reportId: Int) : Response<Unit>
+
+    @PATCH("reports/{reportsId")
+    fun updateReport() : Response<Unit>
 }
