12a58e2132a81c55c71c32783c1b65a8e0eb1381
박장훈 <gretea4489@gmail.com>
2025-05-13 00:05:34 +0900

    [Feat] LoginUseCase Long 타입 반환 변경 #front

--- FitptTrainer/domain/src/main/java/com/ssafy/domain/usercase/auth/LoginUseCase.kt
+++ FitptTrainer/domain/src/main/java/com/ssafy/domain/usercase/auth/LoginUseCase.kt
@@ -8,7 +8,7 @@ import javax.inject.Inject
 class LoginUseCase @Inject constructor(
     private val authRepository: AuthRepository
 ) {
-    suspend operator fun invoke(trainerLoginId: String, trainerPw: String): Flow<ResponseStatus<Unit>> {
+    suspend operator fun invoke(trainerLoginId: String, trainerPw: String): Flow<ResponseStatus<Long>> {
         return authRepository.login(
             trainerLoginId = trainerLoginId,
             trainerPw = trainerPw
