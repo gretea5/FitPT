3dfdb1bd23d6c56f7435766452a93338059bebcb
김동현 <charlie7276@naver.com>
2025-05-13 16:28:25 +0900

    [Feat] MuscleGroup 근육 아이디 값 추가

--- FitptTrainer/domain/src/main/java/com/ssafy/domain/model/report/MuscleGroup.kt
+++ FitptTrainer/domain/src/main/java/com/ssafy/domain/model/report/MuscleGroup.kt
@@ -3,6 +3,7 @@ package com.ssafy.domain.model.report
 import android.widget.ImageView
 
 data class MuscleGroup(
+    val id: Int,
     val key: String,
     val imageViews: List<ImageView>,
     var isSelected: Boolean = false
