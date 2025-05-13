db41981b934e6a47f75c3a27c5aa1757957dcd4d
박장훈 <gretea4489@gmail.com>
2025-05-13 22:35:16 +0900

    [Feat] AuthService 응답 타입 변경 #front

--- FitptTrainer/data/src/main/java/com/ssafy/data/network/api/AuthService.kt
+++ FitptTrainer/data/src/main/java/com/ssafy/data/network/api/AuthService.kt
@@ -1,11 +1,12 @@
 package com.ssafy.data.network.api
 
 import com.ssafy.data.network.request.TrainerLoginRequest
+import com.ssafy.data.network.response.TrainerLoginResponse
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface AuthService {
     @POST("trainers/login")
-    suspend fun login(@Body trainerLoginRequest: TrainerLoginRequest) : Response<Long>
+    suspend fun login(@Body trainerLoginRequest: TrainerLoginRequest) : Response<TrainerLoginResponse>
 }
