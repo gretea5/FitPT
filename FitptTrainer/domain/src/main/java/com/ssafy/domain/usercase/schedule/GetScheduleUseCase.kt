068c6edfbf3bc64a138540e73a96100c5c95d69a
박장훈 <gretea4489@gmail.com>
2025-05-13 16:41:54 +0900

    [Feat] 일정 조회 UseCase 추가 #front

---
+++ FitptTrainer/domain/src/main/java/com/ssafy/domain/usercase/schedule/GetScheduleUseCase.kt
@@ -0,0 +1,25 @@
+package com.ssafy.domain.usercase.schedule
+
+import com.ssafy.domain.model.base.ResponseStatus
+import com.ssafy.domain.model.schedule.Schedule
+import com.ssafy.domain.repository.schedule.ScheduleRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetScheduleUseCase @Inject constructor(
+    private val scheduleRepository: ScheduleRepository
+){
+    suspend operator fun invoke(
+        date: String?,
+        month: String?,
+        trainerId: Long?,
+        memberId: Long?
+    ): Flow<ResponseStatus<List<Schedule>>> {
+        return scheduleRepository.getSchedules(
+            date = date,
+            month = month,
+            trainerId = trainerId,
+            memberId = memberId
+        )
+    }
+}
