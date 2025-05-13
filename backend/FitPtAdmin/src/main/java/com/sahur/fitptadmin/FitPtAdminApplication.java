280262d8e323a66eae2893796ffca69fcbab685b
박장훈 <gretea4489@gmail.com>
2025-05-13 12:44:56 +0900

    [Fix] spring security /login redirect 문제 해결 #back

--- backend/FitPtAdmin/src/main/java/com/sahur/fitptadmin/FitPtAdminApplication.java
+++ backend/FitPtAdmin/src/main/java/com/sahur/fitptadmin/FitPtAdminApplication.java
@@ -2,8 +2,9 @@ package com.sahur.fitptadmin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class FitPtAdminApplication {
 
     public static void main(String[] args) {
