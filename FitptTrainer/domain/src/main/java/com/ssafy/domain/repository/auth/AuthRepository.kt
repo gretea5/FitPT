369a5a4fca72bf06aa99be35a78f386d7dbae173
박장훈 <gretea4489@gmail.com>
2025-05-13 22:33:42 +0900

    [Feat] AuthRepository 리턴 타입 변경 #front

--- FitptTrainer/domain/src/main/java/com/ssafy/domain/repository/auth/AuthRepository.kt
+++ FitptTrainer/domain/src/main/java/com/ssafy/domain/repository/auth/AuthRepository.kt
@@ -1,8 +1,9 @@
 package com.ssafy.domain.repository.auth
 
+import com.ssafy.domain.model.auth.TrainerLogin
 import com.ssafy.domain.model.base.ResponseStatus
 import kotlinx.coroutines.flow.Flow
 
 interface AuthRepository {
-    suspend fun login(trainerLoginId : String, trainerPw: String): Flow<ResponseStatus<Long>>
+    suspend fun login(trainerLoginId : String, trainerPw: String): Flow<ResponseStatus<TrainerLogin>>
 }
