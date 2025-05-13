583a7aedfa99e22f26e48cabc167ab0db5092958
박장훈 <gretea4489@gmail.com>
2025-05-13 16:41:11 +0900

    [Feat] ScheduleService 추가 #front

--- FitptTrainer/data/src/main/java/com/ssafy/data/network/api/ScheduleService.kt
+++ FitptTrainer/data/src/main/java/com/ssafy/data/network/api/ScheduleService.kt
@@ -1,8 +1,16 @@
 package com.ssafy.data.network.api
 
+import com.ssafy.data.network.response.ScheduleResponse
+import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ScheduleService {
     @GET("schedules")
-    suspend fun getSchedules()
+    suspend fun getSchedules(
+        @Query("date") date: String? = null,
+        @Query("month") month: String? = null,
+        @Query("trainerId") trainerId: Long? = null,
+        @Query("memberId") memberId: Long? = null
+    ) : Response<List<ScheduleResponse>>
 }
