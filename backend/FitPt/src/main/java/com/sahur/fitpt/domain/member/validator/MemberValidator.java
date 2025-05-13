e55d56750aec1e8fce559a0869301977f79d5320
안세호 <aash711@naver.com>
2025-05-13 12:40:27 +0900

    [Refactor] member 예외 처리

--- backend/FitPt/src/main/java/com/sahur/fitpt/domain/member/validator/MemberValidator.java
+++ backend/FitPt/src/main/java/com/sahur/fitpt/domain/member/validator/MemberValidator.java
@@ -1,33 +1,46 @@
 package com.sahur.fitpt.domain.member.validator;
 
+import com.sahur.fitpt.core.constant.ErrorCode;
+import com.sahur.fitpt.core.exception.CustomException;
 import com.sahur.fitpt.db.entity.Member;
-import jakarta.persistence.EntityNotFoundException;
+import com.sahur.fitpt.db.repository.TrainerRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class MemberValidator {
+    private final TrainerRepository trainerRepository;
 
     public void validateGender(String gender) {
         if (!gender.equals("남성") && !gender.equals("여성")) {
-            throw new IllegalArgumentException("성별은 '남성' 또는 '여성'이어야 합니다");
+            throw new CustomException(ErrorCode.INVALID_INPUT_VALUE);
+        }
+    }
+
+    public void validateTrainerExists(Long trainerId) {
+        if (!trainerRepository.existsById(trainerId)) {
+            throw new CustomException(ErrorCode.TRAINER_NOT_FOUND);
         }
     }
 
     public void validateMemberExists(Member member, Long memberId) {
         if (member == null) {
-            throw new EntityNotFoundException("ID가 " + memberId + "인 회원을 찾을 수 없습니다");
+            throw new CustomException(ErrorCode.MEMBER_NOT_FOUND);
         }
     }
 
     public void validateHeight(Float height) {
-        if (height != null && (height < 100 || height > 250)) {
-            throw new IllegalArgumentException("키는 100cm에서 250cm 사이여야 합니다");
+        if (height != null && (height <= 100 || height >= 250)) {
+            throw new CustomException(ErrorCode.INVALID_INPUT_VALUE);
         }
     }
 
     public void validateWeight(Float weight) {
-        if (weight != null && (weight < 30 || weight > 250)) {
-            throw new IllegalArgumentException("몸무게는 30kg에서 250kg 사이여야 합니다");
+        if (weight != null && (weight <= 30 || weight >= 250)) {
+            throw new CustomException(ErrorCode.INVALID_INPUT_VALUE);
         }
     }
+
+
 }
