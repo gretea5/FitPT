012b6bcf5019ad93b26fff822772d7f9123c23bd
김동현 <charlie7276@naver.com>
2025-05-13 14:14:20 +0900

    [Feat] HealthReportViewModel 운동 추가 설정 구현 #front

--- FitptTrainer/presentation/src/main/java/com/ssafy/presentation/report/viewmodel/HealthReportViewModel.kt
+++ FitptTrainer/presentation/src/main/java/com/ssafy/presentation/report/viewmodel/HealthReportViewModel.kt
@@ -1,5 +1,8 @@
 package com.ssafy.presentation.report.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -10,5 +13,32 @@ private const val TAG = "HealthReportViewModel_FitPT"
 class HealthReportViewModel @Inject constructor(
     // usercase, datastore 추가 필요
 ) : ViewModel() {
+    private val _name = MutableLiveData<String>("")
+    val name: LiveData<String> = _name
 
+    private val _score = MutableLiveData<String>("")
+    val score: LiveData<String> = _score
+
+    private val _isAddButtonEnabled = MediatorLiveData<Boolean>().apply {
+        addSource(_name) { value = isValidInput(_name.value, _score.value) }
+        addSource(_score) { value = isValidInput(_name.value, _score.value) }
+    }
+    val isAddButtonEnabled: LiveData<Boolean> = _isAddButtonEnabled
+
+    private fun isValidInput(name: String?, score: String?): Boolean {
+        return !name.isNullOrBlank() && !score.isNullOrBlank()
+    }
+
+    fun updateName(newName: String) {
+        _name.value = newName
+    }
+
+    fun updateScore(newScore: String) {
+        _score.value = newScore
+    }
+
+    fun clearInputs() {
+        _name.value = ""
+        _score.value = ""
+    }
 }
