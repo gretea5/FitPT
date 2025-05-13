49759969fadc26eaa7a475dc62496a89493c6804
박장훈 <gretea4489@gmail.com>
2025-05-13 12:00:48 +0900

    [Feat] ScheduleRepositoryImpl 추가 #front

---
+++ FitptTrainer/data/src/main/java/com/ssafy/data/repository/schedule/ScheduleRepositoryImpl.kt
@@ -0,0 +1,11 @@
+package com.ssafy.data.repository.schedule
+
+import com.ssafy.data.network.api.ScheduleService
+import javax.inject.Inject
+
+class ScheduleRepositoryImpl @Inject constructor(
+    private val scheduleService : ScheduleService
+)
+{
+
+}
