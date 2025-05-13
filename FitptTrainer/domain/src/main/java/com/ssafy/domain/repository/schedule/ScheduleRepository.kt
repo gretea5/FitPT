635e9b2bb94bf7630dd58dc64807840dc68ef3c3
박장훈 <gretea4489@gmail.com>
2025-05-13 12:00:25 +0900

    [Feat] ScheduleRepository 생성 #front

---
+++ FitptTrainer/domain/src/main/java/com/ssafy/domain/repository/schedule/ScheduleRepository.kt
@@ -0,0 +1,15 @@
+package com.ssafy.domain.repository.schedule
+
+import com.ssafy.domain.model.schedule.Schedule
+import kotlinx.coroutines.flow.Flow
+
+interface ScheduleRepository {
+    suspend fun getSchedules(
+        date: String? = null,
+        month: String? = null,
+        trainerId: Long? = null,
+        memberId: Long? = null
+    ): Flow<List<Schedule>>
+
+
+}
