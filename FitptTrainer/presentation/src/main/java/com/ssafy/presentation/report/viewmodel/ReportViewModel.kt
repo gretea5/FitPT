10a8ec6d09b7ccda5c85126c8371ba629507aaea
김동현 <charlie7276@naver.com>
2025-05-14 15:22:05 +0900

    [Feat] ReportViewModel 추가 #front

---
+++ FitptTrainer/presentation/src/main/java/com/ssafy/presentation/report/viewmodel/ReportViewModel.kt
@@ -0,0 +1,12 @@
+package com.ssafy.presentation.report.viewmodel
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+private const val TAG = "ReportViewModel_FitPT"
+
+@HiltViewModel
+class ReportViewModel @Inject constructor() : ViewModel() {
+
+}
