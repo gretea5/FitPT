43c407f6e854932454596b00d28b0f77095260b4
박장훈 <gretea4489@gmail.com>
2025-05-14 23:04:07 +0900

    [Feat] MemberRespoitory 생성 #front

---
+++ FitptTrainer/domain/src/main/java/com/ssafy/domain/repository/member/MemberRepository.kt
@@ -0,0 +1,9 @@
+package com.ssafy.domain.repository.member
+
+import com.ssafy.domain.model.base.ResponseStatus
+import com.ssafy.domain.model.member.MemberInfo
+import kotlinx.coroutines.flow.Flow
+
+interface MemberRepository {
+    suspend fun getMemberInfoById(memberId: Long) : Flow<ResponseStatus<MemberInfo>>
+}
