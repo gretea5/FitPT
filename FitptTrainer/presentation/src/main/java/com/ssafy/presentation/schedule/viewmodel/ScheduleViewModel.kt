702a47c023a99e43b242f81e19a01a1184c9568e
박장훈 <gretea4489@gmail.com>
2025-05-16 01:14:19 +0900

    [Feat] SchduleViewModel 일정 생성 함수 추가 #front

--- FitptTrainer/presentation/src/main/java/com/ssafy/presentation/schedule/viewmodel/ScheduleViewModel.kt
+++ FitptTrainer/presentation/src/main/java/com/ssafy/presentation/schedule/viewmodel/ScheduleViewModel.kt
@@ -8,6 +8,7 @@ import com.ssafy.domain.model.base.ResponseStatus
 import com.ssafy.domain.model.member.MemberInfo
 import com.ssafy.domain.model.schedule.Schedule
 import com.ssafy.domain.usecase.member.GetMembersUseCase
+import com.ssafy.domain.usecase.schedule.CreateScheduleUseCase
 import com.ssafy.domain.usecase.schedule.GetScheduleUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -24,6 +25,7 @@ private const val TAG = "ScheduleViewModel_ssafy"
 class ScheduleViewModel @Inject constructor(
     private val getScheduleUseCase: GetScheduleUseCase,
     private val getMembersUseCase: GetMembersUseCase,
+    private val createScheduleUseCase: CreateScheduleUseCase,
     private val dataStore: TrainerDataStoreSource
 ) : ViewModel() {
     private val _scheduleState = MutableStateFlow<ScheduleStatus>(ScheduleStatus.Idle)
@@ -37,6 +39,9 @@ class ScheduleViewModel @Inject constructor(
 
     private val _monthlyScheduleItems = MutableStateFlow<List<Schedule>>(emptyList())
     val monthlyScheduleItems: StateFlow<List<Schedule>> = _monthlyScheduleItems.asStateFlow()
+
+    private val _createdScheduleId = MutableStateFlow<Long?>(null)
+    val createdScheduleId: StateFlow<Long?> = _createdScheduleId.asStateFlow()
 
     fun getSchedules(date: String? = null, month: String? = null, trainerId: Long? = null, memberId: Long? = null) {
         viewModelScope.launch {
@@ -104,6 +109,37 @@ class ScheduleViewModel @Inject constructor(
                 }
             } catch (e: Exception) {
                 Log.d(TAG, "getMembers exception ${e.message}")
+                _scheduleState.value = ScheduleStatus.Error("서버와의 연결에 실패했습니다.")
+            }
+        }
+    }
+
+    fun createSchedule(memberId: Long, startTime: String, endTime: String, scheduleContent: String) {
+        viewModelScope.launch {
+            try {
+                val storedTrainerId = dataStore.trainerId.first()
+
+                createScheduleUseCase(
+                    memberId,
+                    startTime,
+                    endTime,
+                    scheduleContent,
+                    storedTrainerId
+                ).collect { response ->
+                    Log.d(TAG, "createSchedule: $response")
+                    when (response) {
+                        is ResponseStatus.Success -> {
+                            _createdScheduleId.value = response.data
+                            _scheduleState.value = ScheduleStatus.Success
+                        }
+
+                        is ResponseStatus.Error -> {
+                            _scheduleState.value = ScheduleStatus.Error(response.error.message)
+                        }
+                    }
+                }
+            } catch (e: Exception) {
+                Log.d(TAG, "createSchedule exception ${e.message}")
                 _scheduleState.value = ScheduleStatus.Error("서버와의 연결에 실패했습니다.")
             }
         }
