cf7326205f3528011c4cf8460f7368f9255b99b0
박장훈 <gretea4489@gmail.com>
2025-05-16 01:15:14 +0900

    [Feat] TimeUtils 1시간 이후 정보 반환 함수 구현 #front

--- FitptTrainer/presentation/src/main/java/com/ssafy/presentation/util/TimeUtils.kt
+++ FitptTrainer/presentation/src/main/java/com/ssafy/presentation/util/TimeUtils.kt
@@ -1,12 +1,24 @@
 package com.ssafy.presentation.util
 
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
 object TimeUtils {
     fun parseDateTime(dateTimeString: String): Pair<String, String> {
-        // "T"를 기준으로 날짜와 시간 분리
-        val parts = dateTimeString.split("T")
-        val date = parts[0]  // "2025-05-17"
-        val time = parts[1].substring(0, 5)  // "17:30" (초 부분 제거)
+            // "T"를 기준으로 날짜와 시간 분리
+            val parts = dateTimeString.split("T")
+            val date = parts[0]  // "2025-05-17"
+            val time = parts[1].substring(0, 5)  // "17:30" (초 부분 제거)
 
-        return Pair(date, time)
+            return Pair(date, time)
+        }
+
+    fun calculateScheduleTimes(selectedDate: LocalDate, selectedTime: String): Pair<String, String> {
+        val startTime = "${selectedDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))}T${selectedTime}:00"
+
+        val endHour = selectedTime.split(":")[0].toInt() + 1
+        val endTime = "${selectedDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))}T${endHour.toString().padStart(2, '0')}:00:00"
+
+        return Pair(startTime, endTime)
     }
 }
