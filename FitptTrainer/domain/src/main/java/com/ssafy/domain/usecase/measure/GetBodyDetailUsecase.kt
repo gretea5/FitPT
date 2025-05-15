f279572a699290ae69115a30d9d5a156497f0112
김동현 <charlie7276@naver.com>
2025-05-16 08:41:10 +0900

    [Feat] 체성분 리스트 조회, 체성분 상세 조회 usercase 추가 #front

---
+++ FitptTrainer/domain/src/main/java/com/ssafy/domain/usecase/measure/GetBodyDetailUsecase.kt
@@ -0,0 +1,16 @@
+package com.ssafy.domain.usecase.measure
+
+import com.ssafy.domain.model.base.ResponseStatus
+import com.ssafy.domain.model.measure.CompositionDetail
+import com.ssafy.domain.model.measure.CompositionItem
+import com.ssafy.domain.repository.measure.BodyRepository
+import com.ssafy.domain.repository.user.DataStoreRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+
+class GetBodyDetailUsecase @Inject constructor(private val bodyRepository: BodyRepository, private val dataStoreRepository: DataStoreRepository){
+    suspend operator fun invoke(compositionLogId :Int): Flow<ResponseStatus<CompositionItem>> {
+        return bodyRepository.getBodyDetail(compositionLogId)
+    }
+}
