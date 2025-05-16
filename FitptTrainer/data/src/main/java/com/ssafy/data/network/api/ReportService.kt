e510b4978503511c97b816e8172463d19c2955b3
김동현 <charlie7276@naver.com>
2025-05-16 09:26:18 +0900

    [Feat] ReportService 메소드 추가 #front

--- FitptTrainer/data/src/main/java/com/ssafy/data/network/api/ReportService.kt
+++ FitptTrainer/data/src/main/java/com/ssafy/data/network/api/ReportService.kt
@@ -1,11 +1,26 @@
 package com.ssafy.data.network.api
 
 import com.ssafy.domain.model.report.Report
+import com.ssafy.domain.model.report.ReportDetail
+import com.ssafy.domain.model.report.ReportList
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.PATCH
 import retrofit2.http.POST
+import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface ReportService {
+    @GET("reports")
+    suspend fun getReportList(@Query("memberId") memberId: Int): Response<List<ReportList>>
+
     @POST("reports")
     suspend fun createReport(@Body report: Report): Response<Int>
+
+    @GET("reports/{reportId}")
+    suspend fun getReportDetail(@Path("reportId") reportId: Int): Response<ReportDetail>
+
+    @PATCH("reports/{reportId}")
+    suspend fun updateReport(@Path("reportId") reportId: Int, @Body report: Report): Response<Int>
 }
