829333550726bade66233a41257711e4d0935357
박장훈 <gretea4489@gmail.com>
2025-05-16 15:20:37 +0900

    [Feat] ScheduleService 일정 수정 함수 추가 #front

--- FitptTrainer/data/src/main/java/com/ssafy/data/network/api/ScheduleService.kt
+++ FitptTrainer/data/src/main/java/com/ssafy/data/network/api/ScheduleService.kt
@@ -7,6 +7,7 @@ import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.PUT
 import retrofit2.http.Path
 import retrofit2.http.Query
 
@@ -21,6 +22,12 @@ interface ScheduleService {
 
     @POST("schedules")
     suspend fun createSchedule(@Body scheduleRequest: ScheduleRequest) : Response<Long>
+
+    @PUT("schedules/{scheduleId}")
+    suspend fun updateSchedule(
+        @Path("scheduleId") scheduleId: Long,
+        @Body scheduleRequest: ScheduleRequest
+    ) : Response<Long>
 
     @DELETE("schedules/{scheduleId}")
     suspend fun deleteSchedule(@Path("scheduleId") scheduleId: Long) : Response<Long>
