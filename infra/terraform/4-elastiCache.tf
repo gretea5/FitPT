d925ae7259b2d952e90c4f517777b4ae392d251a
권경탁 <kkt3289@gmail.com>
2025-05-29 23:55:17 +0900

    fix Terraform and Ansible

--- infra/terraform/4-elastiCache.tf
+++ infra/terraform/4-elastiCache.tf
@@ -42,22 +42,25 @@ resource "aws_elasticache_parameter_group" "redis_62_param" {
   # }
 }
 
-resource "aws_elasticache_cluster" "redis" {
-  cluster_id           = "${var.project_name}-${var.customer_id}-redis"
-  engine               = "redis"
-  engine_version       = "6.2"
-  node_type            = "cache.t3.micro"
-  num_cache_nodes      = 1
-  parameter_group_name = aws_elasticache_parameter_group.redis_62_param.name
-  port                 = 6379
-  subnet_group_name    = aws_elasticache_subnet_group.redis_subnet.name
-  security_group_ids   = [aws_security_group.redis_sg.id]
+# 비밀번호 설정 레디스와 없는 레디스 리소스 다름
+resource "aws_elasticache_replication_group" "redis" {
+  replication_group_id       = "${var.project_name}-${var.customer_id}-redis"
+  description                = "Redis with AUTH & TLS"   # 변경된 인수명, 필수
+  engine                     = "redis"
+  engine_version             = "6.2"
+  node_type                  = "cache.t3.micro"
+  num_cache_clusters         = 1                        # 변경된 인수명
+  automatic_failover_enabled = false
+
+  auth_token                  = var.redis_auth_token
+  transit_encryption_enabled  = true
+
+  subnet_group_name           = aws_elasticache_subnet_group.redis_subnet.name
+  security_group_ids          = [aws_security_group.redis_sg.id]
 
   tags = merge(
     local.common_tags,
-    {
-      Name = "${var.project_name}-${var.customer_id}-redis"
-    }
+    { Name = "${var.project_name}-${var.customer_id}-redis" }
   )
 }
 
